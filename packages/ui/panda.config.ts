4b86e860434c13f498b909295052eba0004a68b3
woojung Yun <wj.yun@nation-a.com>
2025-03-18 18:52:30 +0900

    chore: fix build error

--- packages/ui/panda.config.ts
+++ packages/ui/panda.config.ts
@@ -2,20 +2,16 @@ import { defineConfig } from '@pandacss/dev'
 import { pandaTokens } from '@nation-a/tokens'
 
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
   syntax: 'object-literal',
   jsxFramework: 'react',
   jsxFactory: 'styled',
   jsxStyleProps: 'all',
 
-  // Where to look for your css declarations
   include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
 
-  // Files to exclude
   exclude: [],
 
-  // Useful for theme customization
   presets: [],
   theme: {
     extend: {
@@ -25,6 +21,5 @@ export default defineConfig({
     },
   },
 
-  // The output directory for your css system
   outdir: 'src/styles/pandacss',
 })
