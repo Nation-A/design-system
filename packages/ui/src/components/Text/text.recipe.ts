9c68c1382980d173e2226c63008a38bbb01d6299
Kyungbae Min <kb.min@nation-a.com>
2025-03-31 19:14:23 +0900

    fix: added fontFamily

--- packages/ui/src/components/Text/text.recipe.ts
+++ packages/ui/src/components/Text/text.recipe.ts
@@ -21,39 +21,51 @@ export const textRecipe = cva({
     variant: {
       'display.lg': {
         textStyle: 'display.lg',
+        fontFamily: 'inter',
       },
       'display.md': {
         textStyle: 'display.md',
+        fontFamily: 'inter',
       },
       'headline.md': {
         textStyle: 'headline.md',
+        fontFamily: 'inter',
       },
       'headline.sm': {
         textStyle: 'headline.sm',
+        fontFamily: 'inter',
       },
       'title.lg': {
         textStyle: 'title.lg',
+        fontFamily: 'notoSans',
       },
       'title.md': {
         textStyle: 'title.md',
+        fontFamily: 'notoSans',
       },
       'title.sm': {
         textStyle: 'title.sm',
+        fontFamily: 'notoSans',
       },
       'body.lg': {
         textStyle: 'body.lg',
+        fontFamily: 'notoSans',
       },
       'body.md': {
         textStyle: 'body.md',
+        fontFamily: 'notoSans',
       },
       'body.sm': {
         textStyle: 'body.sm',
+        fontFamily: 'notoSans',
       },
       'label.sm': {
         textStyle: 'label.sm',
+        fontFamily: 'notoSans',
       },
       'label.md': {
         textStyle: 'label.md',
+        fontFamily: 'notoSans',
       },
     },
   },
