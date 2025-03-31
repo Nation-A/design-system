637e00e660cbbce65e2b247f7566793f564aaf70
yeonkimm <ky.kim@nation-a.com>
2025-03-31 16:38:10 +0900

    feat: default export문 작성

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Text, type TextProps } from './Text'
 export { default as Dialog, type DialogProps } from './Dialog'
 export { default as Spinner, type SpinnerProps } from './Spinner'
 export { default as IconButton, type IconButtonProps } from './IconButton'
-export { default as BottomSheet} from './BottomSheet'
+export { default as BottomSheet } from './BottomSheet'
+export { default as Tag } from './Tag'
 
 export { Portal, type PortalProps } from '@ark-ui/react'
