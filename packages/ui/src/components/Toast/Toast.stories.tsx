759f027e8a21ffe032cc6d0bddaa45accbeb27bc
yeonkimm <ky.kim@nation-a.com>
2025-04-03 20:30:09 +0900

    NDS-23 feat: Toast 스토리북 작성

--- packages/ui/src/components/Toast/Toast.stories.tsx
+++ packages/ui/src/components/Toast/Toast.stories.tsx
@@ -1,35 +1,137 @@
-// import type { Meta, StoryObj } from '@storybook/react'
-// import { HStack, VStack } from '@styled-system/jsx'
-// import { Toast } from './index'
+import type { Meta, StoryObj } from '@storybook/react'
+import Toast from './index'
+import Button from '../Button'
+import toast from '@/utils/toast'
+import { CheckCircleFillIcon } from '@nation-a/icons'
+import { ToastProvider } from '@/contexts'
+import { VStack } from '@styled-system/jsx'
+const meta: Meta<typeof Toast> = {
+  title: 'Components/Toast',
+  component: Toast.Root,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+}
 
-// const meta: Meta<typeof Toast> = {
-//   title: 'Components/Toast',
-//   component: Toast,
-//   parameters: {
-//     layout: 'centered',
-//   },
-//   tags: ['autodocs'],
-// }
+export default meta
+type Story = StoryObj<typeof Toast>
 
-// export default meta
-// type Story = StoryObj<typeof Toast>
+export const DefaultToast: Story = {
+  render: () => {
+    return (
+      <ToastProvider>
+        <VStack gap={5}>
+          <Button onClick={() => toast.show('This is a toast message')}>Show Toast</Button>
+          <Button
+            onClick={() =>
+              toast.show('This is a toast message with icon', {
+                icon: <CheckCircleFillIcon />,
+              })
+            }
+          >
+            Show Toast With Icon
+          </Button>
+          <Button
+            onClick={() =>
+              toast.show(
+                'This is a toast with long text. This is a toast with long text. This is a toast with long text. This is a toast with long text.',
+                {
+                  icon: <CheckCircleFillIcon />,
+                },
+              )
+            }
+          >
+            Show Toast With Long Text
+          </Button>
+        </VStack>
+      </ToastProvider>
+    )
+  },
+}
 
-// export const Default: Story = {
-//   render: () => (
-//     <div style={{ display: 'flex', flexDirection: 'column', gap: '32px' }}>
-//       {/* Solid Variant - Alpha Color */}
-//       <h3>Variant: Solid - Color: Alpha</h3>
-//       <HStack>
-//         <Toast.Root></Toast.Root>
-//       </HStack>
-//     </div>
-//   ),
-//   parameters: {
-//     docs: {
-//       description: {
-//         story:
-//           'Basic TextArea component showing different states (default, selected, disabled) for each style (variant and color).',
-//       },
-//     },
-//   },
-// }
+export const ToastWithAction: Story = {
+  render: () => {
+    return (
+      <ToastProvider>
+        <VStack gap={5}>
+          <Button
+            onClick={() =>
+              toast.show('This is a toast message with action', {
+                actionLabel: 'Label',
+                onActionClick: () => console.log('action clicked'),
+              })
+            }
+          >
+            Show Toast With Action
+          </Button>
+          <Button
+            onClick={() =>
+              toast.show('This is a toast message with icon and action', {
+                icon: <CheckCircleFillIcon />,
+                actionLabel: 'Label',
+                onActionClick: () => console.log('action clicked'),
+                duration: 10000,
+              })
+            }
+          >
+            Show Toast With Icon And Action
+          </Button>
+        </VStack>
+      </ToastProvider>
+    )
+  },
+}
+
+export const ToastWithLink: Story = {
+  render: () => {
+    return (
+      <ToastProvider>
+        <Button
+          onClick={() =>
+            toast.show('This is a toast message', {
+              icon: <CheckCircleFillIcon />,
+              actionLabel: 'Label',
+              onActionClick: () => console.log('action clicked'),
+              asLink: true,
+            })
+          }
+        >
+          Show Toast With Link
+        </Button>
+      </ToastProvider>
+    )
+  },
+}
+
+export const ToastWithCustomDuration: Story = {
+  render: () => {
+    return (
+      <ToastProvider>
+        <VStack gap={5}>
+          <Button onClick={() => toast.show('This is a toast message', {})}>
+            Show Toast With Default Duration (3000ms)
+          </Button>
+          <Button
+            onClick={() =>
+              toast.show('Toast With Custom Duration (10000ms)', {
+                duration: 10000,
+              })
+            }
+          >
+            Show Toast With Custom Duration (10000ms)
+          </Button>
+          <Button
+            onClick={() =>
+              toast.show('Toast With Custom Duration (1000ms)', {
+                duration: 1000,
+              })
+            }
+          >
+            Show Toast With Custom Duration (1000ms)
+          </Button>
+        </VStack>
+      </ToastProvider>
+    )
+  },
+}
