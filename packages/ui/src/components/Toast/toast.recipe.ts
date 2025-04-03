11802777b8d89c55887bb5526906c5a9aac1ba76
yeonkimm <ky.kim@nation-a.com>
2025-04-03 20:27:59 +0900

    NDS-23 feat: Toast recipe 구현

--- packages/ui/src/components/Toast/toast.recipe.ts
+++ packages/ui/src/components/Toast/toast.recipe.ts
@@ -1,63 +1,98 @@
 export type ToastVariantProps = keyof typeof toastRecipe.variantMap
 
-// export const toastRecipe = cva({
-//   base: {
-//     display: 'flex',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     gap: 1,
-//     px: 3,
-//     py: 2,
-//     height: 'fit-content',
-//     minHeight: '48px',
-//     width: 'fit-content',
-//     cursor: 'default',
-//     outline: 'none',
-//     backgroundColor: 'background.neutral.default',
-//     borderRadius: 4,
-//     textStyle: 'body.md',
-//     color: 'text.neutral.default',
-//   },
-//   variants: {},
-//   compoundVariants: [],
-// })
-
 import { toastAnatomy } from '@ark-ui/react'
-import { sva } from '@styled-system/css'
+import { sva } from 'dist/styled-system/css'
 
 export const toastRecipe = sva({
   className: 'toast',
-  slots: toastAnatomy.keys(),
+  slots: [...toastAnatomy.keys(), 'content', 'icon'],
   base: {
     root: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
       backgroundColor: 'background.neutral.default',
-      borderRadius: 4,
+      borderRadius: 16,
       height: 'fit-content',
       minHeight: '48px',
+      width: 'fit-content',
+      maxWidth: '328px',
       overflowWrap: 'anywhere',
-      px: 3,
-      py: 2,
       zIndex: 'overlay',
-      transitionDuration: 'slow',
-      transitionProperty: 'translate, scale, opacity, height',
-      transitionTimingFunction: 'default',
+      fontStyle: 'body.md',
+      color: 'content.neutral.default_inverse.bold',
+      pr: 4,
+      pl: 3,
+      py: 2,
+      boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
+      animation: 'fadeIn 0.25s ease-out',
+      transform: 'translateY(0)',
+      shadow: 'overlay',
+      '&[data-state="open"]': {
+        animation: 'fadeIn 0.25s ease-out',
+        transform: 'translateY(0)',
+      },
+      '&[data-state="closed"]': {
+        animation: 'fadeOut 0.25s ease-out',
+        transform: 'translateY(20px)',
+      },
     },
-    title: {
-      color: 'fg.default',
-      fontWeight: 'semibold',
-      textStyle: 'sm',
+    content: {
+      display: 'inline-flex',
+      alignItems: 'center',
     },
     description: {
-      color: 'fg.muted',
-      textStyle: 'sm',
+      textStyle: 'body.md',
+      marginLeft: 2,
     },
     actionTrigger: {
-      mt: 2,
+      height: '100%',
+      width: 'fit-content',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      textStyle: 'title.sm',
+      cursor: 'pointer',
+      px: 4,
+      pr: 1,
+      whiteSpace: 'nowrap',
+      transition: 'opacity 0.2s ease',
+      '&:hover': {
+        opacity: 0.8,
+      },
     },
-    closeTrigger: {
-      position: 'absolute',
-      top: 3,
-      right: 3,
+    icon: {
+      width: 6,
+      height: 6,
+      minWidth: 6,
+      minHeight: 6,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  },
+  variants: {
+    width: {
+      full: {
+        root: {
+          width: '100%',
+          justifyContent: 'space-between',
+        },
+      },
+      fit: {
+        root: {
+          width: 'fit-content',
+        },
+      },
+    },
+    asLink: {
+      true: {
+        actionTrigger: {
+          textDecoration: 'underline',
+          cursor: 'pointer',
+          color: 'content.neutral.default_inverse.bold',
+        },
+      },
     },
   },
 })
