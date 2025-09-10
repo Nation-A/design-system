a5ab130fab7a73c1485189caa145af9fca864db1
Kyungbae Min <kb.min@nation-a.com>
2025-09-10 15:28:13 +0900

    feat: update checkbox component styles and add new icons

---
+++ .changeset/calm-rings-prove.md
@@ -0,0 +1,6 @@
+---
+'@nation-a/icons': patch
+'@nation-a/ui': patch
+---
+
+Added icons and changed Checkbox component style
--- packages/ui/src/components/Checkbox/index.tsx
+++ packages/ui/src/components/Checkbox/index.tsx
@@ -31,8 +31,8 @@ const CheckIcon = () => (
       d="M11.6666 3.5L5.24992 9.91667L2.33325 7"
       stroke="currentColor"
       strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      strokeLinecap="square"
+      strokeLinejoin="miter"
     />
   </svg>
 )
@@ -40,7 +40,7 @@ const CheckIcon = () => (
 const MinusIcon = () => (
   <svg viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
     <title>Minus Icon</title>
-    <path d="M2.91675 7H11.0834" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M2.91675 7H11.0834" stroke="currentColor" strokeWidth="2" strokeLinecap="square" strokeLinejoin="miter" />
   </svg>
 )
 
